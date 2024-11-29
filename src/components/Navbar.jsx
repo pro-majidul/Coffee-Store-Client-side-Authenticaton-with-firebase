2da4adac4c78eed46bdf6c9232f2344a27120ad3
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-11-29 23:15:29 +0600

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
 import banner from '../assets/images/more/logo1.png'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div className='bg-bgnav bg-no-repeat bg-cover bg-center py-1'>
-            <div className='flex items-center md:w-4/12 w-6/12 mx-auto'>
-                <img className='md:w-16 w-8' src={banner} alt="" />
-                <h3 className='md:text-3xl font-bold text-white'>Espresso Emporium</h3>
+
+            <div className="navbar md:px-12">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <NavLink to='/' className='btn '>Home</NavLink>
+                            <NavLink to='addCoffee' className='btn '>Add Coffee</NavLink>
+                            <NavLink to='/signin' className='btn '>Sign in</NavLink>
+
+                        </ul>
+                    </div>
+                    <div className='flex items-center '>
+                        <img className='md:w-16 w-8' src={banner} alt="" />
+                        <h3 className='md:text-3xl font-bold text-white'>Espresso Emporium</h3>
+                    </div>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 gap-5">
+                        <NavLink to='/' className='btn '>Home</NavLink>
+                        <NavLink to='addCoffee' className='btn '>Add Coffee</NavLink>
+                        <NavLink to='/signin' className='btn '>Sign in</NavLink>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Button</a>
+                </div>
             </div>
         </div>
     );
