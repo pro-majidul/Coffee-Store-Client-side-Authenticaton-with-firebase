45c7c8cf5a72a9af1003d3778ddf11f9f265c104
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-11-30 15:53:26 +0600

    delete user in ui added

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,10 +1,52 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const Users = () => {
     const loaderUser = useLoaderData();
 
     const [users, setUsers] = useState(loaderUser)
+
+    const handeldeleteUser = (id) => {
+        console.log(id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:3000/users/${id}`, {
+                    method: "DELETE",
+                    headers: {
+                        'Content-Type': 'Application/json'
+                    }
+
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            const remainingItems = users.filter(user => user._id !=id);
+                            setUsers(remainingItems);
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+
+                        }
+                    })
+
+
+
+            }
+        });
+    }
+
+
     return (
         <div className='w-11/12 mx-auto my-5 py-5'>
             <div className="overflow-x-auto">
@@ -31,7 +73,7 @@ const Users = () => {
                                 <td>{user.lastSignInTime}</td>
                                 <td className='gap-1 flex'>
                                     <button className='btn btn-success'>edit</button>
-                                    <button className='btn btn-warning'>X</button>
+                                    <button onClick={() => handeldeleteUser(user._id)} className='btn btn-warning'>X</button>
                                 </td>
                             </tr>)
                         }
